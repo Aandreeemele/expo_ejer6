4407a553a68f793dfefdd9c2bb1faf7e9cbe09cd
Santiago <slopez@scl.edu.gt>
2024-05-10 15:59:29 -0600

    actualización del css y js

--- js/header.js
+++ js/header.js
@@ -2,14 +2,15 @@ let div_header = document.querySelector("header");
 div_header.innerHTML =`
     
 <header class="barra">
-    <header class="s1">🖥</header>
-    <header class="s2"><a>📟</a></header>
-    <header class="s3">🏿</header>
-    <header class="s4">🔌</header>
+    <header class="s1">CPU↷</header>
+    <header class="s2"><a>GPU↷</a></header>
+    <header class="s3">MB↷</header>
+    <header class="s4">RAM↷</header>
     <header class="sh">
-        <p class="sh1">Search</p></header>
+        <p class="sea">Computacion@scl.gt</p>
+        </header>
     
-        <img src="https://medialicdn.com/dms/image/C560BAQFReQup08LukQ/company-logo_200_200/0/1652286229007?e=2147483647&v=beta&t=cB65tfGD72TrPKNWt3aSnaNNPKKTzlrgGY4vTAMhpyg" alt="" class="js1">
+        <img src="https://img.freepik.com/vector-gratis/plantilla-logotipo-computadora-creativa_23-2149213537.jpg" alt="" class="zx1">
    
 </header>
 
